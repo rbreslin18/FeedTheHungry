df7dbb119ac3fa8dad3b96b006f106b1537b22fc
Rodney Breslin <33833167+rbreslin18@users.noreply.github.com>
2021-07-14 13:36:15 -0400

    Changed db1,py, main.py, and app.kv
    Added new functions to db1 which allows users to change items in a database using the email
    Main.py and app.kv now include the Admin screen to test functionality

--- db1.py
+++ db1.py
@@ -17,13 +17,14 @@ class DataBase:
     cr.execute("""CREATE TABLE IF NOT EXISTS logins
                 (name TEXT,
                  email TEXT,
-                 password TEXT)""")
+                 password TEXT
+                 admin TEXT)""")
 
     # add user to database
 
-    def add(name, email, password):
+    def add(name, email, password, admin):
         with connect:
-            cr.execute("INSERT INTO logins VALUES(?,?,?)",(name, email, password))
+            cr.execute("INSERT INTO logins VALUES(?,?,?,?)", (name, email, password, admin))
             connect.commit()
             print("user added !")
             
@@ -44,10 +45,15 @@ class DataBase:
                 return True #return true if there is an email and password present with the login given
             else:
                 return False    #return false if no login with those credentials are present
-    def update(email, password):# checks for email to find password
+
+    def validateAdmin(admin):
         with connect:
-            cr.execute('UPDATE logins SET password = ? WHERE password = ?', (password, email)) #set password when email is found in database
-            connect.commit()
+            cr.execute('SELECT * FROM logins WHERE admin = ?', (admin)) #Collect the email and password from database
+            if cr.fetchall(): #check if there is no values
+                return True #return true if there is an email and password present with the login given
+            else:
+                return False    #return false if no login with those credentials are present
+
 
     # read data from database
     def read():
